f1cb0a0030794f44e5d81be834e14b4a1f804240
Alok Shandilya <alok.shandilya10@gmail.com>
2024-05-07 21:52:57 +0530

    add add .sql files solutions

---
+++ 0-querying/2-36_views/10.sql
@@ -0,0 +1,1 @@
+SELECT "english_title" AS "Alok is great" FROM "views" WHERE "artist" = 'Hokusai' ORDER BY "contrast";
---
+++ 0-querying/2-36_views/5.sql
@@ -0,0 +1,1 @@
+SELECT MAX("contrast") AS 'Maximum Constrast' FROM "views" WHERE "artist" = 'Hokusai';
