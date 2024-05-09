4e10e304029216f06b8d52dc080e0add93ca6196
Alok Shandilya <alok.shandilya10@gmail.com>
2024-05-09 10:12:56 +0530

    add third solution

--- 1-relating/packages/log.sql
+++ 1-relating/packages/log.sql
@@ -51,4 +51,26 @@ WHERE "id" = (
 
 
 -- *** The Forgotten Gift ***
+SELECT "contents" FROM "packages"
+WHERE "to_address_id" = (
+  SELECT "id" FROM "addresses"
+  WHERE "address" = '728 Maple Place'
+);
 
+SELECT * FROM "drivers"
+WHERE "id" = (
+  SELECT "driver_id" FROM "scans"
+  WHERE "package_id" = (
+    SELECT "id" FROM "packages"
+    WHERE "from_address_id" = (
+      SELECT "id" FROM "addresses"
+      WHERE "address" = '109 Tileston Street'
+    )
+    AND "to_address_id" = (
+      SELECT "id" FROM "addresses"
+      WHERE "address" = '728 Maple Place'
+    )
+  )
+  ORDER BY "timestamp" DESC
+  LIMIT 1
+);
